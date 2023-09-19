53f99d145028bc9529addb82ee3ee4688dd040e7
Madhukar <madhukarreddy154@gmail.com>
2023-09-19 16:09:39 +0530

    ReactJs

---
+++ react14-reacthooks1/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import StateHook1 from './StateHook1';
+import StateHook2 from './StateHook2';
+
+
+function App() {
+  return (
+    <div className="App">
+        <StateHook1></StateHook1>
+    </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ react16-reacthook3/src/App.js
@@ -0,0 +1,16 @@
+import UseEffectHook1 from './UseEffectHook1';
+import UseEffectHook from './UseEffectHook';
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+         <UseEffectHook/>
+    </div>
+  );
+}
+
+export default App;
